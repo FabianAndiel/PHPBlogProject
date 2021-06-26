a070f3b30073c17a24a9e217b70cad813c71b059
Fabian Andiel <fabian.andiel@yahoo.com>
2021-06-26 15:47:53 +0200

    abstractions work

--- src/Core/AbstractRepository.php
+++ src/Core/AbstractRepository.php
@@ -2,7 +2,40 @@
 
 namespace App\Core;
 
+use PDO; 
+
+
 abstract class AbstractRepository {
+
+    private $pdo; 
+
+
+    abstract public function getTableName();
+
+    abstract public function getModelName();
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function fetchAll(){
+        
+        $posts = $this->getTableName();
+        $model = $this->getModelName();
+        $statement = $this->pdo->query("SELECT * FROM $posts");
+        return $statement->fetchAll(PDO::FETCH_CLASS,$model);
+    }
     
+    public function fetchOne($id){
+        
+        $posts = $this->getTableName();
+        $model = $this->getModelName();
+        $statement = $this->pdo->prepare("SELECT * FROM $posts WHERE id = :id ");
+        $statement->execute(array('id'=> $id));
+        $statement->setFetchMode(PDO::FETCH_CLASS,$model);
+        return $statement->fetch(PDO::FETCH_CLASS);
+    }
+
 
 } 
--- src/Posts/PostsRepository.php
+++ src/Posts/PostsRepository.php
@@ -1,33 +1,16 @@
 <?php
 
 namespace App\Posts;
+use App\Core\AbstractRepository;
 
-use ArrayAccess;
-use PDO;
+class PostsRepository extends AbstractRepository {
 
-class PostsRepository {
-
-     
-    private $pdo; 
-
-    public function __construct($pdo)
-    {
-        $this->pdo = $pdo;
+    public function getTableName() {
+        return 'posts';    
     }
 
-
-    public function fetchAll(){
-
-        $statement = $this->pdo->query("SELECT * FROM `posts`");
-        return $statement->fetchAll(PDO::FETCH_CLASS,PostsModel::class);
-    }
-    
-    public function fetchOne($id){
-    
-        $statement = $this->pdo->prepare("SELECT * FROM `posts` WHERE id = :id ");
-        $statement->execute(array('id'=> $id));
-        $statement->setFetchMode(PDO::FETCH_CLASS,PostsModel::class);
-        return $statement->fetch(PDO::FETCH_CLASS);
+    public function getModelName() {
+        return PostsModel::class;  
     }
 
 }
